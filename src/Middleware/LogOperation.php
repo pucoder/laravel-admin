430ad9e531d2ed1f43b8412625b43c8b18ec91d0
蒲德军 <xiaozhi2015@hotmail.com>
2021-01-04 20:08:38 +0800

    Revert "判断权限再写入日志"
    This reverts commit 5cbca9dd33596546ad70938cc34965edffeb21bc.

--- src/Middleware/LogOperation.php
+++ src/Middleware/LogOperation.php
@@ -22,13 +22,12 @@ class LogOperation
             $request->setTrustedProxies(request()->getClientIps(), Request::HEADER_X_FORWARDED_FOR);
             $logModel = config('admin.database.logs_model');
             $input = $this->desensitization($request->input());
-            $queryString = str_replace('_pjax=%23pjax-container', '', $request->getQueryString());
 
             try {
                 $logModel::create([
                     'user_id' => Admin::user()->id,
                     'operate' => admin_restore_route($request->route()->action['as']),
-                    'path'    => substr(admin_restore_path($request->path() . ($queryString ? '?'.$queryString : '')), 0, 255),
+                    'path'    => substr(admin_restore_path($request->path()), 0, 255),
                     'method'  => $request->method(),
                     'ip'      => $request->getClientIp(),
                     'input'   => json_encode($input),
@@ -48,7 +47,7 @@ class LogOperation
      */
     protected function shouldLogOperation(Request $request)
     {
-        return config('admin.operation_log.enable') && Admin::user() && !$this->inExceptArray($request) && $this->inAllowedMethods($request->method()) && $this->checkPermissions($request);
+        return config('admin.operation_log.enable') && Admin::user() && !$this->inExceptArray($request) && $this->inAllowedMethods($request->method());
     }
 
     /**
@@ -104,19 +103,6 @@ class LogOperation
         return false;
     }
 
-    /**
-     * @param $request
-     * @return bool
-     */
-    protected function checkPermissions($request)
-    {
-        return !config('admin.check_permissions') || (config('admin.check_permissions') && Admin::user()->canAccess($request->route()));
-    }
-
-    /**
-     * @param $input
-     * @return mixed
-     */
     protected function desensitization($input)
     {
         foreach (config('admin.operation_log.desensitization', []) as $value) {
