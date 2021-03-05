eb449354f2a0a5e316414cc82b39c15551e1e884
蒲德军 <xiaozhi2015@hotmail.com>
2021-03-05 18:34:09 +0800

    JStree 调试

--- resources/views/form/jstree.blade.php
+++ resources/views/form/jstree.blade.php
@@ -12,22 +12,67 @@
 
 <script require="jstree" @script>
     $(function() {
+        let related_field = JSON.parse('{!! $relatedField !!}');
+
+        let data_fileds = '';
+
+
+        let core_data = [
+            { "id" : {{ $rootValue }}, "parent" : "#", "text" : "{{ admin_trans('admin.all') }}"}
+        ];
+
+        if (related_field.length > 0) {
+            var related = $('.field-' + related_field[0]);
+            checkRelated(related);
+
+            related.change(function () {
+                $('#{{ $id }}').jstree(true).destroy();// 清除树节点
+                checkRelated(this);
+                $('#{{ $id }}').jstree(true).refresh(); // 刷新树
+            });
+
+            function checkRelated(roles) {
+                $.each($(roles).find('option:selected'), function (key, val) {
+                    data_fileds = $(val).data(related_field[1]);
+                    let data_field = (related_field[1]).split('.');
+                    if (data_field.length > 1) {
+                        data_fileds = $(val).data(data_field[0])[data_field[1]];
+                    }
+                });
+
+                data_fileds = data_fileds.split(',');
+
+                $.each(@json($options), function (key, val) {
+                    if ($.inArray((val.id).toString(), data_fileds) !== -1) {
+                        val.state.selected = true;
+                        val.state.disabled = true;
+                    }
+
+                    core_data.push(val);
+                });
+
+                $('#{{ $id }}').jstree({
+                    'plugins': ["checkbox"],
+                    'core': {
+                        "themes": {
+                            "icons": false,
+                        },
+                        // 'data': [
+                        //     { "id" : 0, "parent" : "#", "text" : "全部"},
+                        //     { "id" : 1, "parent" : 0, "text" : "根节点 1"},
+                        //     { "id" : 2, "parent" : 0, "text" : "根节点 2", 'state' : {'opened' : true}  },
+                        //     { "id" : 3, "parent" : 2, "text" : "子节点 1", 'state' : {'disabled' : true, 'selected' : true} },
+                        //     { "id" : 4, "parent" : 2, "text" : "子节点 2" },
+                        // ]
+                        'data': core_data
+                    }
+                });
+            }
+        }
+
         $('#{{ $id }}').on('changed.jstree', function (e, data) {
-            $(e.currentTarget).next().val(data.selected.toString());
-        })
-        .jstree({
-            'plugins': ["checkbox"],
-            'core': {
-                "themes": {
-                    "icons": false,
-                },
-                // 'data': [
-                //     { "id" : 1, "parent" : "#", "text" : "根节点 1" },
-                //     { "id" : 2, "parent" : "#", "text" : "根节点 2", 'state' : {'opened' : true, 'selected' : true}  },
-                //     { "id" : 3, "parent" : 2, "text" : "子节点 1" },
-                //     { "id" : 4, "parent" : 2, "text" : "子节点 2" },
-                // ]
-                'data': @json($options)
+            if (data.action !== "ready" && data.action !== "model") {
+                $(e.currentTarget).next().val(data.selected.toString());
             }
         });
     });
--- src/Form/Field/JsTree.php
+++ src/Form/Field/JsTree.php
@@ -14,6 +14,39 @@ class JsTree extends Field
     protected $opened = false;
 
     /**
+     * @var array
+     */
+    protected $relatedField = [];
+
+    /**
+     * @var int
+     */
+    protected $rootValue = 0;
+
+    /**
+     * @param string $related
+     * @param string $field
+     * @return $this
+     */
+    public function related($related, $field)
+    {
+        $this->relatedField = [$related, $field];
+
+        return $this;
+    }
+
+    /**
+     * @param int $rootValue
+     * @return $this
+     */
+    public function setRootValue($rootValue = 0)
+    {
+        $this->rootValue = $rootValue;
+
+        return $this;
+    }
+
+    /**
      * @param bool $opened
      * @return $this
      */
@@ -29,10 +62,6 @@ class JsTree extends Field
         $return = [];
 
         foreach ($this->options as $option) {
-            if ($option['parent'] !== '#' && !$option['parent']) {
-                $option['parent'] = '#';
-            }
-
             $opened = false;
             if (in_array((string)$option['id'], explode(',', $this->value))) {
                 $opened = true;
@@ -50,8 +79,10 @@ class JsTree extends Field
     public function render()
     {
         $this->addVariables([
+            'rootValue' => $this->rootValue,
             'value' => $this->value,
             'options' => $this->getOptions(),
+            'relatedField'  => json_encode($this->relatedField),
         ]);
 
         return parent::fieldRender();
