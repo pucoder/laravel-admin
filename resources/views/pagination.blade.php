aa5810486f3e7b867756c689814d61f9c1deeca1
Mr`Pu <xiaozhi2015@hotmail.com>
2021-03-24 16:24:07 +0800

    小屏幕隐藏分页数

--- resources/views/pagination.blade.php
+++ resources/views/pagination.blade.php
@@ -10,16 +10,16 @@
     @foreach ($elements as $element)
     <!-- "Three Dots" Separator -->
     @if (is_string($element))
-    <li class="page-item disabled"><span class="page-link">{{ $element }}</span></li>
+    <li class="page-item disabled hidden-xs"><span class="page-link">{{ $element }}</span></li>
     @endif
 
     <!-- Array Of Links -->
     @if (is_array($element))
     @foreach ($element as $page => $url)
     @if ($page == $paginator->currentPage())
-    <li class="page-item active"><span class="page-link">{{ $page }}</span></li>
+    <li class="page-item active hidden-xs"><span class="page-link">{{ $page }}</span></li>
     @else
-    <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+    <li class="page-item hidden-xs"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
     @endif
     @endforeach
     @endif
